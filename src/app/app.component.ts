5a789c60f0413a2ee85ba4177eb2c6063c370c91
Hermano Lira <hermano.lira@serpro.gov.br>
2021-05-14 13:00:21 -0300

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'req-root',
+  template: `
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'requisicoes-app';
+}
